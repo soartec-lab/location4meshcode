f9a58fd005145de5f439ccb0f3afb058f622f708
soartec-lab <shodai-connect@tbz.t-com.ne.jp>
2018-09-13 14:39:16 +0900

    1次メッシュコード算出メソッド作成

--- lib/location4meshcode/point.rb
+++ lib/location4meshcode/point.rb
@@ -6,5 +6,16 @@ module Location4meshcode
       @latitude = latitude.to_f
       @longitude = longitude.to_f
     end
+
+    def meshcode_level1
+      # 上2桁
+      # 赤道から上方向に40分ごとに振っている通し番号。
+      # 1度(=60分)あたり1.5区画分(60/40 = 1.5)となるので緯度を1.5倍したものが１次メッシュコードの上2桁
+      lat = (@latitude * 1.5).to_i
+      # 下2桁
+      # 東経100度から1度ごとに通し番号を振っているので経度から100を引いたものが１次メッシュコードの下2桁となる。
+      lon = @longitude.to_i - 100
+      "#{lat}#{lon}"
+    end
   end
 end
