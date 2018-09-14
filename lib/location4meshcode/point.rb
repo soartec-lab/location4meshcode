b534cd21faddf1f1e215aaa69dccb6ce3e3b4f54
soartec-lab <shodai-connect@tbz.t-com.ne.jp>
2018-09-14 11:00:55 +0900

    リファクタリング: 処理を細分化

--- lib/location4meshcode/point.rb
+++ lib/location4meshcode/point.rb
@@ -3,7 +3,8 @@ module Location4meshcode
     attr_reader :latitude, :longitude
 
     def initialize(latitude, longitude)
-      @latitude = latitude.to_f
+      # 1度 = 60分なので緯度はここで単位を分に変換しておく
+      @latitude = (latitude.to_f) * 60
       @longitude = longitude.to_f
     end
 
@@ -11,7 +12,7 @@ module Location4meshcode
       # 上2桁
       # 赤道から上方向に40分ごとに振っている通し番号。
       # 1度(=60分)あたり1.5区画分(60/40 = 1.5)となるので緯度を1.5倍したものが1次メッシュコードの上2桁
-      lat = ((@latitude * 60) / 40).to_i
+      lat = (@latitude / 40).to_i
       # 下2桁
       # 東経100度から1度ごとに通し番号を振っているので経度から100を引いたものが1次メッシュコードの下2桁となる。
       lon = @longitude.to_i - 100
@@ -20,22 +21,36 @@ module Location4meshcode
 
     def meshcode_level2
       # 1次メッシュコードの余りを8分区切りにした1つ
-      lat = (((@latitude * 60) % 40) / 5).to_i
-      # 1次メッシュコードの少数点以下を8分区切りにした1つ
-      lon = (((@longitude - @longitude.to_i) * 60) / 7.5).to_i
+      lat = (level1_lat_remainder / 5).to_i
+      lon = (level1_lon_remainder / 7.5).to_i
       # 1次メッシュの結果と結合
       meshcode_level1 + "#{lat}#{lon}"
     end
 
     def meshcode_level3
       # 2次メッシュ区画を10等分
-      lat_remainder = (((@latitude * 60) % 40) % 5)
-      lat = ((lat_remainder * 1) / 0.5).to_i
-
-      lon_remainder = (((@longitude - @longitude.to_i) * 60) % 7.5)
-      lon = ((lon_remainder * 1) / 0.75).to_i
+      lat = ((level2_lat_remainder * 1) / 0.5).to_i
+      lon = ((level2_lon_remainder * 1) / 0.75).to_i
 
       meshcode_level2 + "#{lat}#{lon}"
+    end
+
+    private
+
+    def level1_lat_remainder
+      @latitude % 40
+    end
+
+    def level1_lon_remainder
+      ((@longitude - @longitude.to_i) * 60)
+    end
+
+    def level2_lat_remainder
+      level1_lat_remainder % 5
+    end
+
+    def level2_lon_remainder
+      ((@longitude - @longitude.to_i) * 60) % 7.5
     end
   end
 end
